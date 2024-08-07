bd92f599301519d43456ddf2ccf735427d2a0f60
mukesh307 <156671892+mukesh307@users.noreply.github.com>
2024-08-07 14:38:53 +0530

    url change

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -55,7 +55,7 @@ function App() {
 
   useEffect(() => {
     if (user) {
-      const socketio = io('http://localhost:8000', {
+      const socketio = io('https://instaclone-54jh.onrender.com', {
         query: {
           userId: user?._id
         },
