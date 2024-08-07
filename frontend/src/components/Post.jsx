41fec6c8a8e51a2fa582a66089a9989dcf087cd3
mukesh307 <156671892+mukesh307@users.noreply.github.com>
2024-08-07 13:36:43 +0530

    url change

--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -29,7 +29,7 @@ const Post = ({ post }) => {
     const likeOrDislikeHandler = async () => {
         try {
             const action = liked ? 'dislike' : 'like';
-            const res = await axios.get(`http://localhost:8000/api/v1/post/${post._id}/${action}`, { withCredentials: true });
+            const res = await axios.get(`https://instaclone-54jh.onrender.com/api/v1/post/${post._id}/${action}`, { withCredentials: true });
             if (res.data.success) {
                 const updatedLikes = liked ? postLike - 1 : postLike + 1;
                 setPostLike(updatedLikes);
@@ -51,7 +51,7 @@ const Post = ({ post }) => {
 
     const commentHandler = async () => {
         try {
-            const res = await axios.post(`http://localhost:8000/api/v1/post/${post._id}/comment`, { text }, {
+            const res = await axios.post(`https://instaclone-54jh.onrender.com/api/v1/post/${post._id}/comment`, { text }, {
                 headers: { 'Content-Type': 'application/json' },
                 withCredentials: true
             });
@@ -74,7 +74,7 @@ const Post = ({ post }) => {
 
     const deletePostHandler = async () => {
         try {
-            const res = await axios.delete(`http://localhost:8000/api/v1/post/delete/${post?._id}`, { withCredentials: true })
+            const res = await axios.delete(`https://instaclone-54jh.onrender.com/api/v1/post/delete/${post?._id}`, { withCredentials: true })
             if (res.data.success) {
                 const updatedPostData = posts.filter(postItem => postItem?._id !== post?._id);
                 dispatch(setPosts(updatedPostData));
@@ -88,7 +88,7 @@ const Post = ({ post }) => {
 
     const bookmarkHandler = async () => {
         try {
-            const res = await axios.get(`http://localhost:8000/api/v1/post/${post?._id}/bookmark`, { withCredentials: true });
+            const res = await axios.get(`https://instaclone-54jh.onrender.com/api/v1/post/${post?._id}/bookmark`, { withCredentials: true });
             if (res.data.success) {
                 toast.success(res.data.message);
             }
